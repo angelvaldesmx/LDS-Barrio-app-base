fa88a879640cbb5c1f25f65ce5a1f0813573c6e4
angelvaldesmx <angelmills982@gmail.com>
2025-07-27 14:39:47 -0600

    Actualizar __init__.py

--- backend/routes/__init__.py
+++ backend/routes/__init__.py
@@ -6,5 +6,7 @@ records_bp = Blueprint('records', __name__)
 certificates_bp = Blueprint('certificates', __name__)
 events_bp = Blueprint('events', __name__)
 library_bp = Blueprint('library', __name__)
+app.register_blueprint(uploads_bp, url_prefix='/uploads')
 
+from .uploads import uploads_bp
 from . import auth, members, records, certificates, events, library
