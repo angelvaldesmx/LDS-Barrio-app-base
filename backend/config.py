bae162c2e94d1287ea16afdcd55de6f09939e13f
angelvaldesmx <angelmills982@gmail.com>
2025-07-27 14:28:37 -0600

    Actualizar config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'devkey123')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///lds_barrio.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'clave-secreta')
+    # Llaves extras
+    CERTIFICATE_SECRET_KEY = os.getenv('CERTIFICATE_SECRET_KEY', 'clave_certificados')
