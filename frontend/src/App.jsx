e696ad9fdaf0ecb3f64446b0bb551bb773a0ecc4
angelvaldesmx <angelmills982@gmail.com>
2025-07-27 17:56:22 -0600

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AnimatedRoutes from './router';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App = () => (
+  <>
+    <Navbar />
+    <AnimatedRoutes />
+    <Footer />
+  </>
+);
+
+export default App;
