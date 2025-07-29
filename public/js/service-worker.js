11dd04f5e76b0ca2a14c2354dfd22e8d4b4a7f3d
angelvaldesmx <angelmills982@gmail.com>
2025-07-29 13:12:19 -0600

    Actualizar service-worker.js

--- public/js/service-worker.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js')
-    .then(reg => {
-      console.log('Service Worker registrado con éxito:', reg.scope);
-    })
-    .catch(error => {
-      console.error('Error al registrar el Service Worker:', error);
-    });
-}
