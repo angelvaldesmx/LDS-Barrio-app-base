98e6c7a12af2dbc18cf7eefb07af23f2d744bf5b
angelvaldesmx <angelmills982@gmail.com>
2025-07-29 11:35:32 -0600

    Crear service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'barrio-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/register.html',
+  '/home.html',
+  '/css/styles.css',
+  '/js/main.js',
+  '/images/logo.png',
+  '/manifest.json'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Activación del Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+// Intercepción de peticiones
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
