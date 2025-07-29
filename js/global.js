3eea54f84a546294ef13d365ad896c1195e4ab09
angelvaldesmx <angelmills982@gmail.com>
2025-07-28 23:01:27 -0600

    Crear js/global.js

---
+++ js/global.js
@@ -0,0 +1,12 @@
+// global.js
+
+// Función para manejar foco visible accesible para teclado
+(function () {
+  function handleFirstTab(e) {
+    if (e.key === 'Tab') {
+      document.body.classList.add('user-is-tabbing');
+      window.removeEventListener('keydown', handleFirstTab);
+    }
+  }
+  window.addEventListener('keydown', handleFirstTab);
+})();
