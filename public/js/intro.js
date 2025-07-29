b9493012b0f7717561aaf000eec0cbd272c65b07
angelvaldesmx <angelmills982@gmail.com>
2025-07-29 13:54:57 -0600

    Actualizar intro.js

--- public/js/intro.js
+++ public/js/intro.js
@@ -1,8 +1,8 @@
-function handleIntroAnimation() {
+export function handleIntroAnimation() {
   const lastVisit = localStorage.getItem('lastIntro');
   const now = Date.now();
 
-  if (!lastVisit || now - parseInt(lastVisit) > 86400000) { // 24 horas
+  if (!lastVisit || now - parseInt(lastVisit) > 86400000) {
     includeComponent('intro', '/components/intro.html');
     localStorage.setItem('lastIntro', now.toString());
   }
