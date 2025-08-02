926b48d357aa4683696aa2c598c3e3b508a1111d
angelvaldesmx <angelmills982@gmail.com>
2025-08-02 17:27:18 -0600

    Actualizar utils.js

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,10 +1,12 @@
 export function includeComponent(id, path) {
   const placeholder = document.getElementById(id);
   if (placeholder) {
-    fetch(path)
+    return fetch(path)
       .then(res => res.text())
       .then(html => {
         placeholder.innerHTML = html;
       });
+  } else {
+    return Promise.resolve(); // Si no existe, no hace nada pero no rompe la ejecución
   }
 }
