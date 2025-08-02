21d177e1db685138194646df985bd5f2d66b484a
angelvaldesmx <angelmills982@gmail.com>
2025-08-02 17:26:56 -0600

    Actualizar intro.js

--- public/js/intro.js
+++ public/js/intro.js
@@ -1,11 +1,41 @@
 import { includeComponent } from './utils.js';
 
 export function handleIntroAnimation() {
-  const lastVisit = localStorage.getItem('lastIntro');
-  const now = Date.now();
+  includeComponent('intro', 'components/intro.html').then(() => {
+    const introContainer = document.getElementById("introContainer");
+    const mainContent = document.getElementById("mainContent");
+    const svgObject = document.getElementById("svgIntro");
+    const cerrarBtn = document.getElementById("cerrarIntro");
 
-  if (!lastVisit || now - parseInt(lastVisit) > 86400000) {
-includeComponent('intro', '/components/intro.html');
-    localStorage.setItem('lastIntro', now.toString());
-  }
+    if (!introContainer || !svgObject || !cerrarBtn) return;
+
+    const yaVisto = localStorage.getItem("introVisto");
+
+    function reproducirAnimacionSVG() {
+      svgObject.data = svgObject.data; // Reinicia
+    }
+
+    function cerrarIntro() {
+      introContainer.classList.add("oculto");
+      setTimeout(() => {
+        introContainer.style.display = "none";
+        if (mainContent) mainContent.style.display = "block";
+      }, 1000);
+      localStorage.setItem("introVisto", "true");
+    }
+
+    if (yaVisto === "true") {
+      introContainer.style.display = "none";
+      if (mainContent) mainContent.style.display = "block";
+    } else {
+      reproducirAnimacionSVG();
+      setTimeout(() => {
+        if (introContainer.style.display !== "none") {
+          cerrarIntro();
+        }
+      }, 7000);
+    }
+
+    cerrarBtn.addEventListener("click", cerrarIntro);
+  });
 }
