f1adfa4020673eb8077779e025686f82facf008c
angelvaldesmx <angelmills982@gmail.com>
2025-08-02 17:26:35 -0600

    Actualizar index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,25 +1,8 @@
+import './global.js';
 import { handleIntroAnimation } from './intro.js';
-import './global.js'; // Se importa globalmente para activarlo
 
 document.addEventListener('DOMContentLoaded', () => {
-  // Ejecutar animación de intro si aplica
   handleIntroAnimation();
-
-  // Delegar el clic y timeout solo si el overlay ya fue cargado
-  const observer = new MutationObserver(() => {
-    const introOverlay = document.getElementById('intro-overlay');
-    if (introOverlay) {
-      function hideIntro() {
-        introOverlay.classList.add('hidden');
-      }
-
-      introOverlay.addEventListener('click', hideIntro);
-      setTimeout(hideIntro, 7000);
-      observer.disconnect();
-    }
-  });
-
-  observer.observe(document.body, { childList: true, subtree: true });
 });
 
 // Registro del Service Worker
